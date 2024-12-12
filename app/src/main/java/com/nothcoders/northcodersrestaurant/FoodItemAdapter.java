eb61175876a7c8eae0f921e8a4f129b676b4bc8e
Valdis <v.beikmanis@gmail.com>
2024-12-12 15:37:18 +0000

    feat: task 8-9

--- app/src/main/java/com/nothcoders/northcodersrestaurant/FoodItemAdapter.java
+++ app/src/main/java/com/nothcoders/northcodersrestaurant/FoodItemAdapter.java
@@ -1,8 +1,11 @@
 package com.nothcoders.northcodersrestaurant;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.nothcoders.northcodersrestaurant.databinding.FoodItemLayoutBinding;
@@ -12,12 +15,42 @@ import java.util.List;
 
 public class FoodItemAdapter extends RecyclerView.Adapter<FoodItemAdapter.FoodItemViewHolder> {
     private List<FoodItem> foodItemList;
-    public static class FoodItemViewHolder extends RecyclerView.ViewHolder{
-        private FoodItemLayoutBinding foodItemLayoutBinding;
 
-        public FoodItemViewHolder(@NonNull View itemView, FoodItemLayoutBinding foodItemLayoutBinding) {
-            super(itemView);
-            this.foodItemLayoutBinding = foodItemLayoutBinding;
+    public FoodItemAdapter(List<FoodItem> foodItemList) {
+        this.foodItemList = foodItemList;
+    }
+
+    @NonNull
+    @Override
+    public FoodItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        FoodItemLayoutBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.food_item_layout,
+                parent,
+                false
+        );
+        return new FoodItemViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodItemViewHolder holder, int position) {
+        FoodItem foodItem = foodItemList.get(position);
+        holder.binding.setFoodItem(foodItem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodItemList.size();
+    }
+
+    public static class FoodItemViewHolder extends RecyclerView.ViewHolder{
+        private FoodItemLayoutBinding binding;
+
+        public FoodItemViewHolder(FoodItemLayoutBinding foodItemLayoutBinding) {
+            super(foodItemLayoutBinding.getRoot());
+            this.binding = foodItemLayoutBinding;
         }
     }
+
 }
